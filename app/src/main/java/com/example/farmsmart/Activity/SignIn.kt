3447e6c316098e7a7d572b36ff219fcdd4d51b43
Hitesh <129782600+Hitesh-Meghwal@users.noreply.github.com>
2024-01-25 00:58:29 +0530

    Sign Up page done but some changes left

--- app/src/main/java/com/example/farmsmart/Activity/SignIn.kt
+++ app/src/main/java/com/example/farmsmart/Activity/SignIn.kt
@@ -1,12 +1,20 @@
 package com.example.farmsmart.Activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.example.farmsmart.R
 
 class SignIn : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        val signUp = findViewById<TextView>(R.id.registerSignup)
+        signUp.setOnClickListener {
+            val i = Intent(this,SignUp::class.java)
+            startActivity(i)
+        }
     }
 }
