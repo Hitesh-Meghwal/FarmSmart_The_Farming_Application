f2e470ef87764e7ca7113a4667315a7d097e1564
Hitesh <129782600+Hitesh-Meghwal@users.noreply.github.com>
2024-02-02 21:49:13 +0530

    Forget Password dialog added

--- app/src/main/java/com/example/farmsmart/Activity/SignIn.kt
+++ app/src/main/java/com/example/farmsmart/Activity/SignIn.kt
@@ -1,20 +1,38 @@
 package com.example.farmsmart.Activity
 
+import android.app.Dialog
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
 import android.widget.TextView
 import com.example.farmsmart.R
+import com.example.farmsmart.databinding.ActivitySignInBinding
 
 class SignIn : AppCompatActivity() {
+    lateinit var binding: ActivitySignInBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val signUp = findViewById<TextView>(R.id.registerSignup)
         signUp.setOnClickListener {
             val i = Intent(this,SignUp::class.java)
             startActivity(i)
         }
+        binding.forgetPassword.setOnClickListener {
+            forgetPasswordDialog()
+        }
+    }
+
+    private fun forgetPasswordDialog(){
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.dialog_forgetpassword)
+        dialog.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.show()
     }
 }
--- app/src/main/java/com/example/farmsmart/Activity/SignUp.kt
+++ app/src/main/java/com/example/farmsmart/Activity/SignUp.kt
@@ -1,12 +1,21 @@
 package com.example.farmsmart.Activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.farmsmart.R
+import com.example.farmsmart.databinding.ActivitySignUpBinding
 
 class SignUp : AppCompatActivity() {
+    lateinit var binding: ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.alreadyRegister.setOnClickListener {
+            val goToSignin = Intent(this,SignIn::class.java)
+            startActivity(goToSignin)
+        }
     }
 }
