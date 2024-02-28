0f9f4cc5db938f6134a5f153e2e7ed12abf5b3b3
Hitesh <129782600+Hitesh-Meghwal@users.noreply.github.com>
2024-02-29 01:03:44 +0530

    Popupmenu and Recyclerviewadd to read articles

--- app/src/main/java/com/example/farmsmart/Fragments/BookVehicle.kt
+++ app/src/main/java/com/example/farmsmart/Fragments/BookVehicle.kt
@@ -1,60 +1,64 @@
 package com.example.farmsmart.Fragments
 
 import android.os.Bundle
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import com.example.farmsmart.R
+import com.example.farmsmart.databinding.FragmentBookVehicleBinding
+import com.google.android.material.appbar.MaterialToolbar
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [BookVehicle.newInstance] factory method to
- * create an instance of this fragment.
- */
+@Suppress("UNREACHABLE_CODE", "DEPRECATION")
 class BookVehicle : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    lateinit var binding: FragmentBookVehicleBinding
+    lateinit var toolbar: Toolbar
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_book_vehicle, container, false)
+        binding = FragmentBookVehicleBinding.inflate(inflater,container,false)
+        toolbar = binding.bookVehicleToolbar
+        setHasOptionsMenu(true)
+        toolbar()
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BookVehicle.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BookVehicle().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun toolbar(){
+        toolbar.setOnMenuItemClickListener { menuitem->
+            when(menuitem.itemId){
+                R.id.locationfilter->{
+                    showPopUpMenu()
+                    true
                 }
+                R.id.lowtohigh->{
+                    true
+                }
+                R.id.hightolow ->{
+                    true
+                }
+                else -> false
             }
+        }
     }
+    private fun showPopUpMenu(){
+        val popUpmenu = PopupMenu(requireActivity(), toolbar,Gravity.END)
+        popUpmenu.menuInflater.inflate(R.menu.location_menu,popUpmenu.menu)
+        popUpmenu.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                else -> false
+            }
+        }
+        popUpmenu.show()
+    }
+
+
 }
--- app/src/main/java/com/example/farmsmart/Fragments/Purchase.kt
+++ app/src/main/java/com/example/farmsmart/Fragments/Purchase.kt
@@ -1,60 +1,63 @@
 package com.example.farmsmart.Fragments
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.SearchView.OnQueryTextListener
+import androidx.appcompat.widget.Toolbar
 import com.example.farmsmart.R
+import com.example.farmsmart.databinding.FragmentPurchaseBinding
+import com.google.android.material.appbar.MaterialToolbar
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Purchase.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Purchase : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    lateinit var binding: FragmentPurchaseBinding
+    lateinit var toolbar: MaterialToolbar
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_purchase, container, false)
+        binding = FragmentPurchaseBinding.inflate(inflater,container,false)
+        toolbar = binding.purchaseToolbar
+        toolbar()
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Purchase.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Purchase().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun toolbar(){
+        toolbar.setOnMenuItemClickListener { menuitem->
+            when(menuitem.itemId){
+                R.id.search->{
+                    val searchView = menuitem.actionView as SearchView
+                    searchView.queryHint = "Search Products"
+                    searchView.setOnQueryTextListener(object : OnQueryTextListener{
+                        override fun onQueryTextSubmit(query: String?): Boolean {
+                            return false
+                        }
+                        override fun onQueryTextChange(newText: String?): Boolean {
+                            return false
+                        }
+                    })
+                    true
                 }
+                R.id.history->{
+
+                    true
+                }
+                R.id.cart->{
+
+                    true
+                }
+                else -> false
             }
+        }
     }
 }
