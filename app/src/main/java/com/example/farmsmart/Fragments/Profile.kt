f48ae7d123ee99a9068ad1ccde94b75dfe863e74
Hitesh <129782600+Hitesh-Meghwal@users.noreply.github.com>
2024-03-02 00:08:01 +0530

    In Profile layout toolbar and FAB added

--- app/src/main/java/com/example/farmsmart/Fragments/Profile.kt
+++ app/src/main/java/com/example/farmsmart/Fragments/Profile.kt
@@ -1,60 +1,59 @@
 package com.example.farmsmart.Fragments
 
 import android.os.Bundle
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import com.example.farmsmart.R
+import com.example.farmsmart.databinding.FragmentProfileBinding
+import com.google.android.material.appbar.MaterialToolbar
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Profile.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Profile : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    lateinit var binding: FragmentProfileBinding
+    lateinit var toolbar: MaterialToolbar
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(inflater,container,false)
+        toolbar = binding.profiletoolbar
+        toolbar()
+        return binding.root
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Profile.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Profile().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun toolbar(){
+        toolbar.setOnMenuItemClickListener { menuItem->
+            when(menuItem.itemId){
+                R.id.logout->{
+                    true
                 }
+                R.id.language->{
+                    true
+                }
+                R.id.chatbot->{
+                    true
+                }
+                R.id.popUpMenu->{
+                    val popUpmenu = PopupMenu(requireActivity(),toolbar,Gravity.END)
+                    popUpmenu.menuInflater.inflate(R.menu.profile_popupmenu,popUpmenu.menu)
+                    popUpmenu.setOnMenuItemClickListener {menuItem->
+                        when(menuItem.itemId){
+
+                            else-> false
+                        }
+
+                    }
+                    popUpmenu.show()
+                    true
+                }
+                else -> false
             }
+        }
     }
+
 }
